697284de3249e01e8aec3f5ae7347cfbb28f240b
lenovo <farjadsuhail149@gmail.com>
2023-11-07 19:57:58 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PassingProps from './Components/PassingProps';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>Edit src/App.js and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/PassingProps.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PassingProps() {
+    return <>
+        <h1>abc</h1>
+        <h2>def</h2>
+    </>
+}
+
+export default PassingProps;
